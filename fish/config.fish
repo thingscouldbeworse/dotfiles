c86878be5f6aa6c20fb6b66661b0db5d193458c9
thingscouldbeworse <kirkkhardy@gmail.com>
2026-04-15 14:01:00 -0400

    hyprland, fish, and mimeapps

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+export EDITOR=vim
+export VISUAL=codium
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
